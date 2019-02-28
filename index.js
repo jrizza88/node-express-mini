842ea177777c9751e5aa66a2340d45190fe29eb5
jrizza88 <jetorres88@gmail.com>
2019-02-27 21:00:56 -0500

    completed get request to find specific user using id

--- index.js
+++ index.js
@@ -23,10 +23,27 @@ server.get("/api/users", (req, res) => {
     })
 })
 
+server.get("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    // alt way const { id } = req.params
+    
+    db.findById(id)
+    .then( users => {
+        users ? res.json(users) : res.status(400).json({err: "invalid id!"});
+    })
+    .catch(({code, message}) =>{
+        res.status(code).json({err: message})
+    })
+})
+
 //1. when setting up server, it will need to invoke the methods
 // from the db.js files 
 
 // 2. then you need a promise (use .then and .catch)
+
+// in some cases you will need an if else or ternary function 
+// to compare a successful request or if a user doesn't do somethign right
+// and then you can incorporate the catch!
 
 server.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`)
